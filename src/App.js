27a3359e2ca01b8c91bac93e99d660217355a5c2
bbayong28 <ghkdud5795@hanmail.net>
2022-09-26 12:51:43 +0900

    20220926

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import Header from './pages/Header'
 import Detail from './pages/Detail'
 
 const App = () => {
+    const [con, setCon] = useState();
     useEffect(() => {
-        fetch(process.env.PUBLIC_URL + '/data.json').then(res =>  res.json()).then(res => console.log(res))
+        fetch(process.env.PUBLIC_URL + '/data.json').then(res =>  res.json()).then(res => setCon(res))
     },[])
     return (
-    <div>
+        <div>
+            <div>
+                {/* 자스니까 {} 해주기!!!! */}
+                {
+                !con ? <div>없네?</div>
+                    : <div>{con[1].id}</div>
+                }              
+            </div>
           <Header />
           <Routes>
               <Route path='/list/:num' element={<Detail />} />
           </Routes>
-    </div>
+        </div>
   )
 }
 
